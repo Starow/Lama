1164624007ec17e7d71fc5856847f73cbee62829
Staross <le.bassiste.575@gmail.com>
2013-12-31 02:52:40 +0100

    ajout de la fonction leet

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python2.7
 # -*-coding:UTF-8 -*
 
-import string
 from traitement import *
-from itertools import chain, islice
 
-def morceaux(iterable, taille, format=iter):
-	"""DOC morceaux: Parcours d'iterable par morceaux"""
-	it = iter(iterable)
-	while True:
-		yield format(chain((it.next(),), islice(it, taille - 1)))
+output_file_name = "output"
+input_file_name = "input"
+input_file_name = raw_input("Path to the input file: ")
+output_file_name = raw_input("Path to the output file (created if not exist): ")
 
 #with pour eviter les prob d'ouverture et fermeture.
-with open("fichier","r") as list_pers:
+with open(input_file_name,"r") as list_pers:
 	for morceau in morceaux(list_pers, 5, tuple):
 		#traitement ...
-		traitement(morceau[0][:len(morceau[0])-1],morceau[1][:len(morceau[1])-1],morceau[2][:len(morceau[2])-1],morceau[3][:len(morceau[3])-1],morceau[4][:len(morceau[4])-1])
+		traitement(morceau[0][:len(morceau[0])-1],morceau[1][:len(morceau[1])-1],morceau[2][:len(morceau[2])-1],morceau[3][:len(morceau[3])-1],morceau[4][:len(morceau[4])-1],output_file_name)
 		print morceau
--- traitement.py
+++ traitement.py
@@ -1,15 +1,71 @@
-"""Je suis le module de traitement"""
+"""Je suis le module des fonctions de traitement"""
+from itertools import chain, islice
 
-def traitement(surname,name,birthdate,hometown,postalcode,*empty):
+def morceaux(iterable, taille, format=iter):
+	"""DOC morceaux: Parcours d'iterable par morceaux"""
+	it = iter(iterable)
+	while True:
+		yield format(chain((it.next(),), islice(it, taille - 1)))
+
+def leet(chaine):
+	"""Je suis la fonction de transformation en alphabet leet"""
+	chaine = chaine.lower()
+	chaine = chaine.replace("o","0").replace("i","1").replace("e","3").replace("a","4").replace("s","5").replace("t","7").replace("b","8")
+	return chaine
+
+def traitement(surname,name,birthdate,hometown,postalcode,output,*empty):
 	"""Je suis la fonction de traitement"""
-	print("###Traitement###")
 	day = birthdate[:2]
 	month = birthdate[3:5]
 	year = birthdate[6:]
-
+	short_post = postalcode[:2]
 
 	# ouverture en mode ajout.
-	with open("output","a") as wordlist_mdp:
+	with open(output,"a") as wordlist:
 		#rules of password creations...
-		wordlist_mdp.write(surname+name+month+day+"\n")
-		wordlist_mdp.write(surname+day+"\n")
+		# TODO find an permutation algorithm for this job
+
+		wordlist.write(surname+"\n")
+		wordlist.write(name+"\n")
+		wordlist.write(surname+name+"\n")
+		wordlist.write(name+surname+"\n")
+
+		wordlist.write(day+surname+"\n")
+		wordlist.write(day+name+"\n")
+		wordlist.write(day+surname+name+"\n")
+		wordlist.write(day+name+surname+"\n")
+
+		wordlist.write(surname+day+"\n")
+		wordlist.write(name+day+"\n")
+		wordlist.write(surname+name+day+"\n")
+		wordlist.write(name+surname+day+"\n")
+
+		wordlist.write(surname+year+"\n")
+		wordlist.write(name+year+"\n")
+		wordlist.write(surname+name+year+"\n")
+		wordlist.write(name+surname+year+"\n")
+
+		wordlist.write(year+surname+"\n")
+		wordlist.write(year+name+"\n")
+		wordlist.write(year+surname+name+"\n")
+		wordlist.write(year+name+surname+"\n")
+
+		wordlist.write(surname+short_post+"\n")
+		wordlist.write(name+short_post+"\n")
+		wordlist.write(surname+name+short_post+"\n")
+		wordlist.write(name+surname+short_post+"\n")
+
+		wordlist.write(short_post+surname+"\n")
+		wordlist.write(short_post+name+"\n")
+		wordlist.write(short_post+surname+name+"\n")
+		wordlist.write(short_post+name+surname+"\n")
+
+		#leet one
+		wordlist.write(leet(surname)+"\n")
+		wordlist.write(leet(name)+"\n")
+		wordlist.write(leet(surname+name)+"\n")
+		wordlist.write(leet(name+surname)+"\n")
+
+
+		
+		
