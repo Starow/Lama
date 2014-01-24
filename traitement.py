82efd56f639c3e9810ea34e85f4823782fb735ea
Staross <le.bassiste.575@gmail.com>
2014-01-24 10:49:39 +0100

    Change the Lama Fonction [while, for] stuffs

--- traitement.py
+++ traitement.py
@@ -1,6 +1,7 @@
 """Je suis le module des fonctions de traitement"""
 import sys
 import re
+import itertools
 from itertools import chain, islice, permutations
 
 def Windowing(iterable, taille, format=iter):
@@ -17,7 +18,7 @@ def add_entry(path_input):
 	d_ex = r"^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$"
 	p_ex = r"^(0[1-9]|[1-9][0-9])[0-9]{3}$"
 
-	# Ouverture en mode ajout.
+	# Opening in add mode
 	with open(path_input+'.lama',"a") as list_pers:
 		
 		#while user have data to record
@@ -67,48 +68,50 @@ def leet(chaine):
 	return chaine
 
 
-def all_perms(elements):
-	"""Permutation Function
-	Input : String or list
-	Output: String or list"""
-	if len(elements) <=1:
-		yield elements
-	else:
-		for perm in all_perms(elements[1:]):
-			for i in range(len(elements)):
-				#nb elements[0:1] works in both string and list contexts
-				yield perm[:i] + elements[0:1] + perm[i:]
-
-
-def Lama(morceau, output):
+def Lama(list_raw, output):
 	"""Password Function
 	Input : List, Filename,
 	Output: .lama"""
 
 	#Removing the "\n"
-	for i, elt in enumerate(morceau):
-		morceau[i] = morceau[i][:len(morceau[i])-1]
+	for i, elt in enumerate(list_raw):
+		list_raw[i] = list_raw[i][:len(list_raw[i])-1]
 
 	#Separating day from month from year
-	list_nb_date = [morceau[2][:2],morceau[2][3:5],morceau[2][6:],morceau[2][8:]]
+	list_nb_date = [list_raw[2][:2],list_raw[2][3:5],list_raw[2][6:],list_raw[2][8:]]
 	#Putting postal code and short postal code in a separated list
-	list_cp = [morceau[4],morceau[4][:2]]
+	list_cp = [list_raw[4],list_raw[4][:2]]
+
+	list_name = [list_raw[0],list_raw[1]]
 	#Deleting raw date format
-	del morceau[2]
+	#del list_raw[2]
 	#Deleting the name of the city *** Dunno what to do with it
-	del morceau[2]
-	#Concat date to morceau
-	#morceau.extend(date)
+	#del list_raw[2]
+	#Concat date to list_raw
+	#list_raw.extend(date)
 	# short_post = postalcode[:2]
-	print morceau
+	print list_raw
+
+	print list_name
 	print list_nb_date
 	print list_cp
 
-
 #Uncomplete
-#TODO little permutation...
-#TODO Create intelligent little list to permutate.
+
 	with open(output+'.lama', "a") as wordlist:
-		for perm in all_perms(morceau):
-			wordlist.write(perm[0]+perm[1]+perm[2]+"\n")
+		x = 2
+		#TODO create a function of this : input : list_raw
+		while x <= len(list_raw):
+			for perm in list(itertools.permutations(list_raw, x)):
+				chain = ""
+				y = x - 1
+				while y >= 0:
+					chain = chain + perm[y]
+					y -= 1
+				wordlist.write(chain + "\n")
+			x += 1
+
+		for i,elt in list(itertools.product(list_name, list_nb_date)):
+			print i, elt
+			#wordlist.write(perm[0]+perm[1]+perm[2]+"\n")
 			#wordlist.write(leet(perm[0]+perm[1]+perm[2])+"\n")
