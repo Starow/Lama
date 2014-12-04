f8a820e9382c45fbf05122032d56fd38ffa56824
Debra Jules <jules.debra@conostix.com>
2014-12-04 16:54:06 +0100

    pep8 & replacing tab by space

--- bin/main.py
+++ bin/main.py
@@ -4,47 +4,47 @@
 import argparse
 from traitement import *
 
+
 def main():
-	"""Main Function"""
+    """Main Function"""
+    parser = argparse.ArgumentParser(description='Create passwords wordlist.',
+                                     epilog='Copyright: Staross & Benesaii // 2014')
 
-	parser = argparse.ArgumentParser(description = 'Create passwords wordlist.',
-        epilog = 'Copyright: Staross & Benesaii // 2014')
+    parser.add_argument('input',
+                        type=str,
+                        help='Input file name. If not exist the file will be created',
+                        action='store')
 
-	parser.add_argument('input',
-        type = str,
-        help = 'Input file name',
-        action = 'store')
+    parser.add_argument('output',
+                        type=str,
+                        help='Output file name',
+                        action='store')
 
-	parser.add_argument('output',
-        type = str,
-        help = 'Output file name',
-        action = 'store')
+    parser.add_argument('--add',
+                        help='Add manual entries to the input file',
+                        action='store_true')
 
-	parser.add_argument('--add',
-        help = 'Add manual entries',
-        action = 'store_true')
+    parser.add_argument('--l33t',
+                        help='Toggle the leet treatment',
+                        action='store_true')
 
-	parser.add_argument('--l33t',
-        help = 'Toggle the leet treatment',
-        action = 'store_true')
+    parser.add_argument('--mlen',
+                        default=24,
+                        type=int,
+                        help='Define the max lenght of pass created (default = 24)',
+                        action='store')
 
-	parser.add_argument('--mlen',
-        default = 24,
-        type = int,
-        help = 'Define the max lenght of pass created (default = 24)',
-        action = 'store')
+    args = parser.parse_args()
 
-	args = parser.parse_args()
+    if args.add is True:
+        add_entry(args.input)
 
-	if args.add == True:
-		add_entry(args.input)
-
-	#with pour les prob d'open/close.
-	with open(args.input+'.lama',"r") as list_pers:
-		for morceau in Windowing(list_pers, 5, list):
-			#traitement du fichier input...
-			#TODO traitement de l'erreur si le fichier input est incomplet
-			Lama(morceau, args.output, args.mlen, args.l33t)
+    #  with pour les prob d'open/close.
+    with open(args.input + '.lama', "r") as list_pers:
+            for morceau in Windowing(list_pers, 5, list):
+                #  traitement du fichier input...
+                    #  TODO traitement de l'erreur si le fichier input est incomplet
+                    Lama(morceau, args.output, args.mlen, args.l33t)
 
 if __name__ == "__main__":
     main()
--- bin/traitement.py
+++ bin/traitement.py
@@ -7,117 +7,120 @@ import itertools
 import os
 from itertools import chain, islice, permutations
 
+
 def Windowing(iterable, taille, format=iter):
-	"""Windowing Function"""
-	it = iter(iterable)
-	while True:
-		yield format(chain((it.next(),), islice(it, taille - 1)))
+    """Windowing Function"""
+    it = iter(iterable)
+    while True:
+        yield format(chain((it.next(),), islice(it, taille - 1)))
+
 
 def add_entry(path_input):
-	"""Adding Entry Function
-	Input : Filename
-	Output: .lama"""
+    """Adding Entry Function
+        Input : Filename
+        Output: .lama"""
 
-	d_ex = r"^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$"
-	p_ex = r"^(0[1-9]|[1-9][0-9])[0-9]{3}$"
+    d_ex = r"^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$"
+    p_ex = r"^(0[1-9]|[1-9][0-9])[0-9]{3}$"
 
-	# Opening in add mode
-	with open(path_input+'.lama',"a") as list_pers:
+    # Opening in add mode
+    with open(path_input + '.lama', "a") as list_pers:
+        #  while user have data to record
+        while True:
+            #  Creating a temporary list to store inputs.
+            temp_list = []
 
-		#while user have data to record
-		while True:
-			#Creating a temporary list to store inputs.
-			temp_list = []
+            print "Press enter now to stop"
+            temp_list.append(raw_input("Surname []: "))
 
-			print "Press enter now to stop"
-			temp_list.append(raw_input("Surname []: "))
+            if len(temp_list[0]) == 0:
+                break
 
-			if len(temp_list[0]) == 0:
-				break
+            temp_list.append(raw_input("Name []: "))
 
-			temp_list.append(raw_input("Name []: "))
+            while True:
+                temp_list.append(raw_input("Birthdate dd/mm/yyyy []:"))
+                if re.match(d_ex, temp_list[2]) is not None:
+                    break
+                print "Birthdate Format is not correct"
 
-			while True:
-				temp_list.append(raw_input("Birthdate dd/mm/yyyy []:"))
-				if re.match(d_ex, temp_list[2]) is not None:
-					break
-				print "Birthdate Format is not correct"
+            temp_list.append(raw_input("Hometown []:"))
 
-			temp_list.append(raw_input("Hometown []:"))
+            while True:
+                temp_list.append(raw_input("Postalcode xxxxx []:"))
+                if re.match(p_ex, temp_list[4]) is not None:
+                    break
+                print "Postalcode Format is not correct"
 
-			while True:
-				temp_list.append(raw_input("Postalcode xxxxx []:"))
-				if re.match(p_ex, temp_list[4]) is not None:
-					break
-				print "Postalcode Format is not correct"
+            #  Adding input in the file
+            for i, elt in enumerate(temp_list):
+                list_pers.write(elt + "\n")
 
-			#Adding input in the file
-			for i, elt in enumerate(temp_list):
-				list_pers.write(elt + "\n")
+            #  Deleting the temporary list
+            del temp_list
 
-			#Deleting the temporary list
-			del temp_list;
 
 def leet(chaine):
-	"""Leet Function:
-	Input : string
-	Output: string"""
-	dico_leet = { "a":"4", "e":"3", "i":"1", "s":"5", "t":"7", "b":"8", "o":"0" }
-	chaine = chaine.lower()
+    """Leet Function:
+        Input : string
+        Output: string"""
+    dico_leet = {"a": "4", "e": "3", "i": "1", "s": "5", "t": "7", "b": "8", "o": "0"}
+    chaine = chaine.lower()
 
-	for i, elt in dico_leet.items():
-		chaine = chaine.replace(i,elt)
+    for i, elt in dico_leet.items():
+        chaine = chaine.replace(i, elt)
 
-	return chaine
+    return chaine
 
-def write_all_perm(Alist,Afile,Alen):
-	"""Permutation Function:
-	Input: list, namefile, int, boolean
-	Output: file
-	"""
-	x = 2
-	while x <= len(Alist):
-		for perm in list(itertools.permutations(Alist, x)):
-			chain = ""
-			y = x - 1
-			while y >= 0:
-				chain = chain + perm[y]
-				y -= 1
-				if len(chain) <= Alen :
-					Afile.write(chain + "\n")
-		x += 1
+
+def write_all_perm(Alist, Afile, Alen):
+    """Permutation Function:
+    Input: list, namefile, int, boolean
+    Output: file
+    """
+    x = 2
+    while x <= len(Alist):
+        for perm in list(itertools.permutations(Alist, x)):
+            chain = ""
+            y = x - 1
+            while y >= 0:
+                chain = chain + perm[y]
+                y -= 1
+                if len(chain) <= Alen:
+                    Afile.write(chain + "\n")
+    x += 1
+
 
 def Lama(list_raw, output, mlen, l33t):
-	"""Password Function
-	Input : List, Filename,
-	Output: .lama"""
+    """Password Function
+    Input : List, Filename,
+    Output: .lama"""
 
-	#Removing the "\n"
-	for i, elt in enumerate(list_raw):
-		list_raw[i] = list_raw[i][:len(list_raw[i])-1]
+    #  Removing the "\n"
+    for i, elt in enumerate(list_raw):
+        list_raw[i] = list_raw[i][:len(list_raw[i]) - 1]
 
+    #  If there is an error about the size of the list.
+    try:
+        #  Separating day from month from year.
+        list_nb_date = [list_raw[2][:2], list_raw[2][3:5], list_raw[2][6:], list_raw[2][8:]]
+        #  Putting postal code and short postal code in a separated list.
+        list_cp = [list_raw[4], list_raw[4][:2]]
+        #  Putting Name and surname in a separated list.
+        list_name = [list_raw[0], list_raw[1]]
+    except IndexError:
+        print """the Input list isn't complete (missing line):
+        ==>> Running operation aborted! <<=="""
+        sys.exit(0)
 
-	#If there is an error about the size of the list.
-	try:
-		#Separating day from month from year.
-		list_nb_date = [list_raw[2][:2],list_raw[2][3:5],list_raw[2][6:],list_raw[2][8:]]
-		#Putting postal code and short postal code in a separated list.
-		list_cp = [list_raw[4],list_raw[4][:2]]
-		#Putting Name and surname in a separated list.
-		list_name = [list_raw[0],list_raw[1]]
-	except IndexError:
-		print """the Input list isn't complete (missing line):
-		==>> Running operation aborted! <<=="""
-		sys.exit(0)
+    #  TODO : improve the quality of the wordlist by creating better lists.
+    # Here there is some "useless permutation"
 
-	#TODO : improve the quality of the wordlist by creating better lists.
-	# Here there is some "useless permutation"
+    list_name.extend(list_nb_date)
+    list_name.extend(list_cp)
 
-	list_name.extend(list_nb_date)
-	list_name.extend(list_cp)
+    with open(output + '.lama', "a") as wordlist:
+        write_all_perm(list_name, wordlist, mlen)
 
-	with open(output+'.lama', "a") as wordlist:
-		write_all_perm(list_name,wordlist,mlen)
-
-	Mo = os.path.getsize('./{0}.lama'.format(output)) / 1048576
-	print "A Lama-wordlist {0}.lama was successfully created ({1} Mo)".format(output,Mo)
+    Mo = os.path.getsize('./{0}.lama'.format(output)) / 1048576
+    print "A Lama-wordlist {0}.lama was successfully created ({1} Mo)".format(output, Mo)
